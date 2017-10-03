2e2a0c3233ab9edeb648904f6c37f077cce3ae66
Matthew Huebert <mhuebert@gmail.com>
2017-10-03 17:09:23 +0200

    Publish error-humanizing code under maria.friendly. Update Lark deps, add format-code command (close #21). Use cljs 1.9.946 (close #57).

--- shapes/project.clj
+++ shapes/project.clj
@@ -1,4 +1,4 @@
-(defproject shapes "0.1.0-SNAPSHOT"
+(defproject maria/shapes "0.1.0-SNAPSHOT"
   :description "A shapes library"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
